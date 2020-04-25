d6739b02ba112166122ad95827f8844656a88146
DianaTerchea <44743846+DianaTerchea@users.noreply.github.com>
2020-04-25 20:54:59 +0300

    Stats page "pretty"

--- GetLastComms.js
+++ GetLastComms.js
@@ -1,40 +1,62 @@
 function getLastComms() {
 
-	var xhttp;
-	var postID = "112510383726603_128873248756983"; // Cel mai probabil pe viitor vom salva id-ul postarii in functie de cea pe care o alege utilizatorul (TBD)
-	var url = "https://web-rfnl5hmkocvsi.azurewebsites.net/FBFINAL/REST.php?do=last3comments&postId=" + postID + "&fbid=" + sessionStorage.getItem("id_user");
+    var xhttp;
+    var postID = "112510383726603_128873248756983"; // Cel mai probabil pe viitor vom salva id-ul postarii in functie de cea pe care o alege utilizatorul (TBD)
+    var url = "https://web-rfnl5hmkocvsi.azurewebsites.net/FBFINAL/REST.php?do=last3comments&postId=" + postID + "&fbid=" + sessionStorage.getItem("id_user");
 
-	console.log(url);
+    console.log(url);
 
-	var displayed = 0; 
+    var displayed = 0;
 
-	if (window.XMLHttpRequest) {
-		xhttp = new XMLHttpRequest()
-	} else {
-		xhttp = new ActiveXObject('Microsoft.XMLHTTP')
-	};
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest()
+    } else {
+        xhttp = new ActiveXObject('Microsoft.XMLHTTP')
+    };
 
-	xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function() {
 
-		if (this.readyState == 4 && this.status == 200)
-		{
-			console.log(xhttp.response);
-			var obj = JSON.parse(this.responseText);
-			alert_message = "";
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(xhttp.response);
+            var obj = JSON.parse(this.responseText);
+            alert_message = "";
 
-			for (var i=0; i < obj.COMMENTS.length; i++) {
-				var temp = obj.COMMENTS[i];
-				alert_message = alert_message + temp[0] + " Postat de: " + temp[1] + "<br/>";
-				//console.log(temp);
-			}
+            removeChilds();
+            for (var i = 0; i < obj.COMMENTS.length; i++) {
+                var temp = obj.COMMENTS[i];
+                alert_message = alert_message + temp[0] + " Postat de: " + temp[1];
+                modifyHTML(temp[0], temp[1]);
+                //console.log(temp);
+            }
 
-			console.log(alert_message);
-			//alert(alert_message);
+            console.log(alert_message);
+            //alert(alert_message);
 
-			document.getElementById("divLocation").innerHTML = alert_message;
-		}
-	};
 
-	xhttp.open('GET', url, true);
-	xhttp.send(null);
+        }
+    };
+
+    xhttp.open('GET', url, true);
+    xhttp.send(null);
+}
+
+function modifyHTML(comentariu, owner) {
+    var elem = document.createElement("div")
+    var heading = document.createElement("h4");
+    var comment = document.createElement("p");
+    var comm_text = document.createTextNode(comentariu);
+    var node = document.createTextNode('@' + owner);
+    heading.appendChild(node);
+    comment.appendChild(comm_text);
+    elem.appendChild(heading);
+    elem.appendChild(comment);
+    var original = document.getElementById("content-area");
+    original.append(elem);
+}
+
+function removeChilds() {
+    const myNode = document.getElementById("content-area");
+    while (myNode.lastElementChild) {
+        myNode.removeChild(myNode.lastElementChild);
+    }
 }
--- get-post-text.js
+++ get-post-text.js
@@ -3,40 +3,52 @@ function getPostText() {
     var xhttp;
     var fbid = sessionStorage.getItem("id_user");
     var postid = "112510383726603_128873248756983";
-	var url = "https://web-rfnl5hmkocvsi.azurewebsites.net/FBFINAL/REST.php?do=getText&postId=" + postid + "&fbid=" + fbid ;
+    var url = "https://web-rfnl5hmkocvsi.azurewebsites.net/FBFINAL/REST.php?do=getText&postId=" + postid + "&fbid=" + fbid;
     console.log(url);
-    
-	var displayed = 0; 
 
-	if (window.XMLHttpRequest) {
-		xhttp = new XMLHttpRequest()
-	} else {
-		xhttp = new ActiveXObject('Microsoft.XMLHTTP')
-	};
+    var displayed = 0;
 
-	xhttp.onreadystatechange = function() {
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest()
+    } else {
+        xhttp = new ActiveXObject('Microsoft.XMLHTTP')
+    };
 
-		if (this.readyState == 4 && this.status == 200)
-		{
-			console.log(xhttp.response);
-			var obj = JSON.parse(this.responseText);
-			alert_message ="MESAJUL: " + obj.MESSAGE;
-			console.log(alert_message);
-			//alert(alert_message);
+    xhttp.onreadystatechange = function() {
 
-			document.getElementById("divLocation").innerHTML = alert_message;
-        }
-        else
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(xhttp.response);
+            var obj = JSON.parse(this.responseText);
+            removeChilds();
+            var comentariu = obj.MESSAGE;
+            console.log('Textul postului' + comentariu);
+            modificareHTML(comentariu);
+
+        } else
         if (this.status == 404) {
             if (displayed == 0) {
-              alert('No information found')
-              location.reload()
-              displayed++
+                alert('No information found')
+                location.reload()
+                displayed++
             }
-          }
+        }
 
-	};
+    };
 
-	xhttp.open('GET', url, true);
-	xhttp.send(null);
+    xhttp.open('GET', url, true);
+    xhttp.send(null);
+}
+
+function modificareHTML(comentariu) {
+    var elem = document.createElement("div")
+    var heading = document.createElement("h4");
+    var comment = document.createElement("p");
+    var comm_text = document.createTextNode(comentariu);
+    var node = document.createTextNode('Post:');
+    heading.appendChild(node);
+    comment.appendChild(comm_text);
+    elem.appendChild(heading);
+    elem.appendChild(comment);
+    var original = document.getElementById("content-area");
+    original.append(elem);
 }
