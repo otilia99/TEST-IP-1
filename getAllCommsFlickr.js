6df7b0f9d62bc308f1d1a907c3d97317a79fbd11
vlado1270 <andreivladserban@gmail.com>
2020-05-02 18:15:14 +0300

    Bugs fixed + automation of best post

--- getAllCommsFlickr.js
+++ getAllCommsFlickr.js
@@ -1,8 +1,9 @@
 function getAllCommsFlickr () {
   
     var xhttp
-    var postID = '49765475798'
-    var url = 'https://web-rfnl5hmkocvsi.azurewebsites.net/DPZ/REST.php?do=last3comments&postId=' + postID + '&userid=' + sessionStorage.getItem('id_user')
+    //var postID = '49765475798'
+    var postID = sessionStorage.getItem("postID_FLICKR"); // VLADOO
+    var url = 'https://web-rfnl5hmkocvsi.azurewebsites.net/DPZ/REST.php?do=getComments&postId=' + postID + '&userid=' + sessionStorage.getItem('id_user')
   
     console.log(url)
   
@@ -16,13 +17,23 @@ function getAllCommsFlickr () {
       if (this.readyState == 4 && this.status == 200) {
         console.log(xhttp.response)
         var obj = JSON.parse(this.responseText)
+        console.log("...............")
+        console.log(obj)
         alert_message = ''
   
         removeChildren()
-        for (var i = 0; i < obj.length; i++) {
-          alert_message = alert_message + 'Author: ' + obj[i].AUTHOR + ' AuthorId: ' + obj[i].AUTHORID + ' Comment: ' + obj[i].COMMENT + '\n'
-          modifyHTML(obj[i].COMMENT, obj[i].AUTHOR)
+
+        for (var i = 0; i < obj.COMMENTS.length; i++){
+          //console.log(obj.COMMENTS[i].AUTHOR, obj.COMMENTS[i].AUTHORID, obj.COMMENTS[i].COMMENT)
+          alert_message = alert_message + 'Author: ' + obj.COMMENTS[i].AUTHOR + ' Comment: ' + obj.COMMENTS[i].COMMENT + '\n';
+          modifyHTML(obj.COMMENTS[i].COMMENT, obj.COMMENTS[i].AUTHOR);
+
         }
+
+        /*for (var i = 0; i < obj.COMMENTS.length; i++) {
+          alert_message = alert_message + 'Author: ' + obj.COMMENTS[i].AUTHOR + ' AuthorId: ' + obj.COMMENTS[i].AUTHORID + ' Comment: ' + obj.COMMENTS[i].COMMENT + '\n'
+          modifyHTML(obj[i].COMMENT, obj[i].AUTHOR)
+        }*/
         console.log(alert_message)
         // alert(alert_message);*/
       }
