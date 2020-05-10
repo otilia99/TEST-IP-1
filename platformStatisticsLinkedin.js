fe997f56ed9400e207161ce3ff47c3a02453d4f6
vladluc99 <62203002+vladluc99@users.noreply.github.com>
2020-05-10 22:07:23 +0300

    Linkedin
    Linkedin debug

--- platformStatisticsLinkedin.js
+++ platformStatisticsLinkedin.js
@@ -1,85 +1,96 @@
-function retrieveStats() {
-    const invocation = new XMLHttpRequest()
-    const url = 'https://sma-a4.herokuapp.com/'
-        // http://sma-a4.herokuapp.com/nume_platforma/profile
-    const platforma = 'linkedin/' // se seteaza in functie de ce e nevoie
-    const actiune = 'profile/posts/' // se seteaza in functie de ce e nevoie
+function getStatisticsLinkedin() {
+    const invocation = new XMLHttpRequest();
+    const url = "https://sma-a4.herokuapp.com/";
+    //http://sma-a4.herokuapp.com/nume_platforma/profile
+    const nume_platforma = "linkedin/"
+    const actiune = "profile/posts/stats"
 
-    if (!invocation) return
-    const requestDataLogin = `email=${sessionStorage.getItem('current_email')}&password=${sessionStorage.getItem('current_pass')}`
+    if (!invocation) return;
+    const requestDataLogin = `email=${sessionStorage.getItem("current_email")}&password=${sessionStorage.getItem("current_pass")}`;
 
-    invocation.open('POST', url + 'auth/login', true)
+    invocation.open("POST", url + "auth/login", true);
     invocation.setRequestHeader(
-        'Content-type',
-        'application/x-www-form-urlencoded'
-    )
+        "Content-type",
+        "application/x-www-form-urlencoded"
+    );
 
-    invocation.withCredentials = true
+    invocation.withCredentials = true;
 
-    var count = 0
-    var tempo = 0
+    var count = 0;
+    var tempo = 0;
+
+    console.log(url);
 
     invocation.onreadystatechange = function() {
-        if (tempo == 4) { // jeez christ bad code af dar asigura ca veti primi doar raspunsul la final (json-ul pe care-l asteptati) --speram
-            // console.log(this.response)
-            var responseLines = this.responseText.split('\n')
-            console.log(responseLines[1])
-            var obj = JSON.parse(responseLines[1])
-            removeChildren()
-            modifyHTMLStats(obj.comments_avg, obj.comments_sum, obj.likes_avg, obj.likes_sum, obj.shares_avg, obj.shares_sum)
-        } else { tempo++ }
+        if (tempo == 4) {
+            //console.log(this.response);
+            var responseLines = this.responseText.split("\n");
+            // console.log(responseLines[1]);	
+            var obj = JSON.parse(responseLines[1]);
+
+            removeChildren();
+
+            alert_message = '';
+
+            alert_message = alert_message + 'AVG_COMM: ' + obj.comments_avg + '\n' + 'SUM_COMM: ' + obj.comments_sum + '\n' + 'likes_avg: ' + obj.likes_avg + '\n' + 'Likes_sum: ' + obj.likes_sum + '\n' + 'shares_avg: ' + obj.shares_avg + '\n' + 'shares_sum: ' + obj.shares_sum;
+            modifyHTML(obj.comments_avg, obj.comments_sum, obj.likes_sum, obj.likes_avg, obj.shares_sum, obj.shares_avg);
+
+            console.log(alert_message);
+        } else
+            tempo++;
         if (count == 0) {
-            count = 1
-            this.open('GET', url + platforma + actiune + 'stats', true)
-            this.send()
+            count = 1;
+            this.open("GET", url + nume_platforma + actiune, true);
+            this.send();
+        }
+    };
+
+    invocation.send(requestDataLogin);
+
+    function modifyHTML(comments_avg, comments_sum, likes_sum, likes_avg, shares_sum, shares_avg) {
+        var elem = document.createElement('div')
+        var heading = document.createElement('h4')
+        var comments_avg_p = document.createElement('p')
+        var comments_sum_p = document.createElement('p')
+        var likes_avg_p = document.createElement('p')
+        var likes_sum_p = document.createElement('p')
+        var shares_sum_p = document.createElement('p')
+        var shares_avg_p = document.createElement('p')
+
+        var node = document.createTextNode('Post Stats')
+        var comments_avg_n = document.createTextNode('Average number comments: ' + comments_avg)
+        var comments_sum_n = document.createTextNode('Sum number comments: ' + comments_sum)
+        var likes_avg_n = document.createTextNode('Average number likes: ' + likes_avg)
+        var likes_sum_n = document.createTextNode('Sum number likes: ' + likes_sum)
+        var shares_sum_n = document.createTextNode('Sum number shares: ' + shares_sum)
+        var shares_avg_n = document.createTextNode('Average number shares: ' + shares_avg)
+
+        heading.appendChild(node)
+        comments_avg_p.appendChild(comments_avg_n)
+        comments_sum_p.appendChild(comments_sum_n)
+        likes_avg_p.appendChild(likes_avg_n)
+        likes_sum_p.appendChild(likes_sum_n)
+        shares_sum_p.appendChild(shares_sum_n)
+        shares_avg_p.appendChild(shares_avg_n)
+
+        elem.appendChild(heading)
+        elem.appendChild(comments_avg_p)
+        elem.appendChild(comments_sum_p)
+        elem.appendChild(likes_avg_p)
+        elem.appendChild(likes_sum_p)
+        elem.appendChild(shares_avg_p)
+        elem.appendChild(shares_sum_p)
+
+        var original = document.getElementById('content-area')
+        original.append(elem)
+    }
+
+    function removeChildren() {
+        const myNode = document.getElementById('content-area')
+        while (myNode.lastElementChild) {
+            myNode.removeChild(myNode.lastElementChild)
         }
     }
 
-    invocation.send(requestDataLogin)
-}
 
-function modifyHTMLStats(comments_avg, comments_sum, likes_avg, likes_sum, shares_avg, shares_sum) {
-    var elem = document.createElement('div')
-    var heading = document.createElement('h4')
-    var comments_avg_p = document.createElement('p')
-    var comments_sum_p = document.createElement('p')
-    var likes_avg_p = document.createElement('p')
-    var likes_sum_p = document.createElement('p')
-    var shares_avg_p = document.createElement('p')
-    var shares_sum_p = document.createElement('p')
-
-
-    var heading_n = document.createTextNode('Your account stats: ')
-    var comments_avg_n = document.createTextNode('Average number of comments: ' + comments_avg)
-    var comments_sum_n = document.createTextNode('Total number of comments: ' + comments_sum)
-    var likes_avg_n = document.createTextNode('Average number of likes: ' + likes_avg)
-    var likes_sum_n = document.createTextNode('Total number of likes: ' + likes_sum)
-    var shares_avg_n = document.createTextNode('Average number of shares: ' + shares_avg)
-    var shares_sum_n = document.createTextNode('Total number of shares: ' + shares_sum)
-
-    heading.appendChild(heading_n)
-    comments_avg_p.appendChild(comments_avg_n)
-    comments_sum_p.appendChild(comments_sum_n)
-    likes_avg_p.appendChild(likes_avg_n)
-    likes_sum_p.appendChild(likes_sum_n)
-    shares_avg_p.appendChild(shares_avg_n)
-    shares_sum_p.appendChild(shares_sum_n)
-
-    elem.appendChild(heading)
-    elem.appendChild(comments_avg_p)
-    elem.appendChild(comments_sum_p)
-    elem.appendChild(likes_avg_p)
-    elem.appendChild(likes_sum_p)
-    elem.appendChild(shares_avg_p)
-    elem.appendChild(shares_sum_p)
-
-    var original = document.getElementById('content-area')
-    original.append(elem)
-}
-
-function removeChildren() {
-    const myNode = document.getElementById('content-area')
-    while (myNode.lastElementChild) {
-        myNode.removeChild(myNode.lastElementChild)
-    }
 }
--- retrieveProfileLinkedin.js
+++ retrieveProfileLinkedin.js
@@ -1,9 +1,9 @@
-function retrieveProfileTwitter() {
+function retrieveProfileLinkedin() {
     const invocation = new XMLHttpRequest();
     const url = "https://sma-a4.herokuapp.com/";
     //http://sma-a4.herokuapp.com/nume_platforma/profile
-    const nume_platforma = "twitter/" // se seteaza in functie de ce e nevoie	
-    const actiune = "profile" // se seteaza in functie de ce e nevoie
+    const nume_platforma = "linkedin/"
+    const actiune = "profile"
 
     if (!invocation) return;
     const requestDataLogin = `email=${sessionStorage.getItem("current_email")}&password=${sessionStorage.getItem("current_pass")}`;
@@ -19,105 +19,49 @@ function retrieveProfileTwitter() {
     var count = 0;
     var tempo = 0;
 
-    invocation.onreadystatechange = function() {
-        if (tempo == 4) { // jeez christ bad code af dar asigura ca veti primi doar raspunsul la final (json-ul pe care-l asteptati)
-            //console.log(this.response);
-            // raspunsul o sa contina 1. raspunsul de la login si 2. raspunsul asteptat de voi (urmeaza sa facem rost doar de cel asteptat de voi)
-            var responseLines = this.responseText.split("\n");
-            console.log(responseLines[1]); // cu asta o sa lucrati -> NU SUNT SIGUR CA O SA FUNCTIONEZE IN TOATE CAZURILE (EX: CAND PRIMITI UN ARRAY?) DECI VERIFICATI CE SE SALVEAZA IN ACEASATA DATA CU TOT RASPUNSUL PLES!
-            // continuati cu ce trebuie facut
+    //console.log(url);
 
+    invocation.onreadystatechange = function() {
+        if (tempo == 4) {
+            var responseLines = this.responseText.split("\n");
+            console.log(responseLines[1]);
+            var obj = JSON.parse(responseLines[1]);
+            console.log(obj.id);
+
+            removeChildren();
+
+            alert_message = ''
+
+            alert_message = alert_message + 'Name: ' + obj.name + '\n' + 'ID: ' + obj.id + '\n' + 'Followers: ' + obj.followers + '\n';
+            modifyHTML(obj.name, obj.id, obj.followers);
+
+
+            console.log(alert_message);
         } else
             tempo++;
         if (count == 0) {
             count = 1;
-            this.open("GET", url + nume_platforma + actiune + "/posts", true);
+            this.open("GET", url + nume_platforma + actiune, true);
             this.send();
         }
     };
 
     invocation.send(requestDataLogin);
 
-}
-
-function modifyHTMLProfile(owner, comentariu, id) {
-    var elem = document.createElement('div')
-    var heading = document.createElement('h4')
-    var comment = document.createElement('p')
-    var id_p = document.createElement('p')
-    var comm_text = document.createTextNode('Followers: ' + comentariu)
-    var id_node = document.createTextNode('Id: ' + id)
-    var node = document.createTextNode('@' + owner)
-    heading.appendChild(node)
-    comment.appendChild(comm_text)
-    id_p.appendChild(id_node)
-    elem.appendChild(heading)
-    elem.appendChild(comment)
-    elem.appendChild(id_p)
-    var original = document.getElementById('content-area')
-    original.append(elem)
-}
-
-function removeChildren() {
-    const myNode = document.getElementById('content-area')
-    while (myNode.lastElementChild) {
-        myNode.removeChild(myNode.lastElementChild)
-    }
-
-    function retrieveProfileTwitter() {
-        const invocation = new XMLHttpRequest()
-        const url = 'https://sma-a4.herokuapp.com/'
-            // http://sma-a4.herokuapp.com/nume_platforma/profile
-        const nume_platforma = 'twitter/' // se seteaza in functie de ce e nevoie
-        const actiune = 'profile' // se seteaza in functie de ce e nevoie
-
-        if (!invocation) return
-        const requestDataLogin = `email=${sessionStorage.getItem('current_email')}&password=${sessionStorage.getItem('current_pass')}`
-
-        invocation.open('POST', url + 'auth/login', true)
-        invocation.setRequestHeader(
-            'Content-type',
-            'application/x-www-form-urlencoded'
-        )
-
-        invocation.withCredentials = true
-
-        var count = 0
-        var tempo = 0
-
-        invocation.onreadystatechange = function() {
-            if (tempo == 4) {
-                var responseLines = this.responseText.split('\n')
-                console.log(responseLines[1])
-                var obj = JSON.parse(responseLines[1])
-                removeChildren()
-                modifyHTMLProfile(obj.name, obj.followers, obj.id)
-
-            } else { tempo++ }
-            if (count == 0) {
-                count = 1
-                this.open('GET', url + nume_platforma + actiune, true)
-                this.send()
-            }
-        }
-
-        invocation.send(requestDataLogin)
-    }
-
-    function modifyHTMLProfile(owner, comentariu, id) {
+    function modifyHTML(nume, id, followers) {
         var elem = document.createElement('div')
         var heading = document.createElement('h4')
         var comment = document.createElement('p')
-        var id_p = document.createElement('p')
-        var comm_text = document.createTextNode('Followers: ' + comentariu)
-        var id_node = document.createTextNode('Id: ' + id)
-        var node = document.createTextNode('@' + owner)
+        var followers_nr = document.createElement('p')
+        var comm_text = document.createTextNode('ID: ' + id)
+        var node = document.createTextNode('@' + nume)
+        var follow = document.createTextNode('Followers: ' + followers)
         heading.appendChild(node)
         comment.appendChild(comm_text)
-        id_p.appendChild(id_node)
+        followers_nr.appendChild(follow);
         elem.appendChild(heading)
         elem.appendChild(comment)
-        elem.appendChild(id_p)
+        elem.appendChild(followers_nr);
         var original = document.getElementById('content-area')
         original.append(elem)
     }
@@ -128,4 +72,6 @@ function removeChildren() {
             myNode.removeChild(myNode.lastElementChild)
         }
     }
+
+
 }
