18d7ddd8233364066a3f815f34c09f3ba668e5fc
otilia99 <45422133+otilia99@users.noreply.github.com>
2020-05-12 18:55:43 +0300

    token for fb + retrieveFollowersCount for linkedin+fb+twitter

--- get-post-text.js
+++ get-post-text.js
@@ -1,55 +1,55 @@
 function getPostText() {
 
-    var xhttp;
-    var fbid = sessionStorage.getItem("id_user");
-    //var postid = "112510383726603_128873248756983";
-    var postid = sessionStorage.getItem("postID_FB");
-    var url = "https://web-rfnl5hmkocvsi.azurewebsites.net/FBFINAL/REST.php?do=getText&postId=" + postid + "&fbid=" + fbid;
-    console.log(url);
+  var xhttp;
+  var token = sessionStorage.getItem("token");
+  //var postid = "112510383726603_128873248756983";
+  var postid = sessionStorage.getItem("postID_FB");
+  var url = "https://web-rfnl5hmkocvsi.azurewebsites.net/FBFINAL/REST.php?do=getText&postId=" + postid + "&jwt=" + token;
+  console.log(url);
 
-    var displayed = 0;
+  var displayed = 0;
 
-    if (window.XMLHttpRequest) {
-        xhttp = new XMLHttpRequest()
-    } else {
-        xhttp = new ActiveXObject('Microsoft.XMLHTTP')
-    };
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest()
+  } else {
+    xhttp = new ActiveXObject('Microsoft.XMLHTTP')
+  };
 
-    xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function() {
 
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(xhttp.response);
-            var obj = JSON.parse(this.responseText);
-            removeChilds();
-            var comentariu = obj.MESSAGE;
-            console.log('Textul postului' + comentariu);
-            modificareHTML(comentariu);
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.response)
+      var obj = JSON.parse(this.responseText)
+      removeChilds()
+      var comentariu = obj.MESSAGE
+      console.log('Textul postului' + comentariu)
+      modificareHTML(comentariu)
 
-        } else
-        if (this.status == 404) {
-            if (displayed == 0) {
-                alert('No information found')
-                location.reload()
-                displayed++
-            }
-        }
+    } else
+    if (this.status == 404) {
+      if (displayed == 0) {
+        alert('No information found')
+        location.reload()
+        displayed++
+      }
+    }
 
-    };
+  };
 
-    xhttp.open('GET', url, true);
-    xhttp.send(null);
+  xhttp.open('GET', url, true);
+  xhttp.send(null);
 }
 
 function modificareHTML(comentariu) {
-    var elem = document.createElement("div")
-    var heading = document.createElement("h4");
-    var comment = document.createElement("p");
-    var comm_text = document.createTextNode(comentariu);
-    var node = document.createTextNode('Post:');
-    heading.appendChild(node);
-    comment.appendChild(comm_text);
-    elem.appendChild(heading);
-    elem.appendChild(comment);
-    var original = document.getElementById("content-area");
-    original.append(elem);
+  var elem = document.createElement("div")
+  var heading = document.createElement("h4");
+  var comment = document.createElement("p");
+  var comm_text = document.createTextNode(comentariu);
+  var node = document.createTextNode('Post:');
+  heading.appendChild(node);
+  comment.appendChild(comm_text);
+  elem.appendChild(heading);
+  elem.appendChild(comment);
+  var original = document.getElementById("content-area");
+  original.append(elem);
 }
---
+++ retrieveDailyFollowers.js
@@ -0,0 +1,225 @@
+function retrieveFollowersTwitter() {
+  const invocation = new XMLHttpRequest()
+  const url = 'https://sma-a4.herokuapp.com/'
+  // http://sma-a4.herokuapp.com/nume_platforma/profile
+  const platforma = 'twitter/' // se seteaza in functie de ce e nevoie
+  const actiune = 'profile/' // se seteaza in functie de ce e nevoie
+
+  if (!invocation) return
+  const requestDataLogin = `email=${sessionStorage.getItem('current_email')}&password=${sessionStorage.getItem('current_pass')}`
+
+  invocation.open('POST', url + 'auth/login', true)
+  invocation.setRequestHeader(
+    'Content-type',
+    'application/x-www-form-urlencoded'
+  )
+
+  invocation.withCredentials = true
+
+  var count = 0
+  var tempo = 0
+
+  invocation.onreadystatechange = function() {
+    if (tempo == 4) { // jeez christ bad code af dar asigura ca veti primi doar raspunsul la final (json-ul pe care-l asteptati)
+      // console.log(this.response)
+      var responseLines = this.responseText.split('\n')
+      console.log(responseLines[1])
+      var obj = JSON.parse(responseLines[1])
+      removeChildren()
+      for(var i = 0; i < obj.followers_count.length; i++){
+        modifyHTMLTwit(obj.followers_count[i].automatic, obj.followers_count[i].followers, obj.followers_count[i].timestamp)
+    }
+    } else { tempo++ }
+    if (count == 0) {
+      count = 1
+      console.log(url+platforma+actiune+'stats')
+      this.open('GET', url + platforma + actiune + 'stats', true)
+      this.send()
+    }
+  }
+
+  invocation.send(requestDataLogin)
+}
+
+function modifyHTMLTwit(automatic, followers, timestamp) {
+  var elem = document.createElement('div')
+  var heading = document.createElement('h4')
+  var automatic_p = document.createElement('p')
+  var followers_p = document.createElement('p')
+  var timestamp_p = document.createElement('p')
+
+  var heading_n = document.createTextNode('Your daily followers count: ')
+  var automatic_n = document.createTextNode('Automatic: ' + automatic)
+  var followers_n = document.createTextNode('Number of followers: ' + followers)
+  var timestamp_n = document.createTextNode('Timestamp: ' + timestamp)
+
+  heading.appendChild(heading_n)
+  automatic_p.appendChild(automatic_n)
+  followers_p.appendChild(followers_n)
+  timestamp_p.appendChild(timestamp_n)
+
+  elem.appendChild(heading)
+  elem.appendChild(automatic_p)
+  elem.appendChild(followers_p)
+  elem.appendChild(timestamp_p)
+
+  var original = document.getElementById('content-area')
+  original.append(elem)
+}
+
+function removeChildren() {
+  const myNode = document.getElementById('content-area')
+  while (myNode.lastElementChild) {
+    myNode.removeChild(myNode.lastElementChild)
+  }
+}
+
+function retrieveFollowersTumblr() {
+    const invocation = new XMLHttpRequest()
+    const url = 'https://sma-a4.herokuapp.com/'
+    // http://sma-a4.herokuapp.com/nume_platforma/profile
+    const platforma = 'tumblr/' // se seteaza in functie de ce e nevoie
+    const actiune = 'profile/' // se seteaza in functie de ce e nevoie
+  
+    if (!invocation) return
+    const requestDataLogin = `email=${sessionStorage.getItem('current_email')}&password=${sessionStorage.getItem('current_pass')}`
+  
+    invocation.open('POST', url + 'auth/login', true)
+    invocation.setRequestHeader(
+      'Content-type',
+      'application/x-www-form-urlencoded'
+    )
+  
+    invocation.withCredentials = true
+  
+    var count = 0
+    var tempo = 0
+  
+    invocation.onreadystatechange = function() {
+      if (tempo == 4) { // jeez christ bad code af dar asigura ca veti primi doar raspunsul la final (json-ul pe care-l asteptati)
+        // console.log(this.response)
+        var responseLines = this.responseText.split('\n')
+        console.log(responseLines[1])
+        var obj = JSON.parse(responseLines[1])
+        removeChildren()
+        for(var i = 0; i < obj.followers_count.length; i++){
+            modifyHTMLTumblr(obj.followers_count[i].automatic, obj.followers_count[i].followers, obj.followers_count[i].timestamp)
+        }
+      } else { tempo++ }
+      if (count == 0) {
+        count = 1
+        this.open('GET', url + platforma + actiune + 'stats', true)
+        this.send()
+      }
+    }
+  
+    invocation.send(requestDataLogin)
+  }
+  
+  function modifyHTMLTumblr(automatic, followers, timestamp) {
+    var elem = document.createElement('div')
+    var heading = document.createElement('h4')
+    var automatic_p = document.createElement('p')
+    var followers_p = document.createElement('p')
+    var timestamp_p = document.createElement('p')
+  
+    var heading_n = document.createTextNode('Your daily followers count: ')
+    var automatic_n = document.createTextNode('Automatic: ' + automatic)
+    var followers_n = document.createTextNode('Number of followers: ' + followers)
+    var timestamp_n = document.createTextNode('Timestamp: ' + timestamp)
+  
+    heading.appendChild(heading_n)
+    automatic_p.appendChild(automatic_n)
+    followers_p.appendChild(followers_n)
+    timestamp_p.appendChild(timestamp_n)
+  
+    elem.appendChild(heading)
+    elem.appendChild(automatic_p)
+    elem.appendChild(followers_p)
+    elem.appendChild(timestamp_p)
+  
+    var original = document.getElementById('content-area')
+    original.append(elem)
+  }
+  
+  function removeChildren() {
+    const myNode = document.getElementById('content-area')
+    while (myNode.lastElementChild) {
+      myNode.removeChild(myNode.lastElementChild)
+    }
+  }
+  
+  function retrieveFollowersLinked() {
+    const invocation = new XMLHttpRequest()
+    const url = 'https://sma-a4.herokuapp.com/'
+    // http://sma-a4.herokuapp.com/nume_platforma/profile
+    const platforma = 'linkedin/' // se seteaza in functie de ce e nevoie
+    const actiune = 'profile/' // se seteaza in functie de ce e nevoie
+  
+    if (!invocation) return
+    const requestDataLogin = `email=${sessionStorage.getItem('current_email')}&password=${sessionStorage.getItem('current_pass')}`
+  
+    invocation.open('POST', url + 'auth/login', true)
+    invocation.setRequestHeader(
+      'Content-type',
+      'application/x-www-form-urlencoded'
+    )
+  
+    invocation.withCredentials = true
+  
+    var count = 0
+    var tempo = 0
+  
+    invocation.onreadystatechange = function() {
+      if (tempo == 4) { // jeez christ bad code af dar asigura ca veti primi doar raspunsul la final (json-ul pe care-l asteptati)
+        // console.log(this.response)
+        var responseLines = this.responseText.split('\n')
+        console.log(responseLines[1])
+        var obj = JSON.parse(responseLines[1])
+        removeChildren()
+        for(var i = 0; i < obj.followers_count.length; i++){
+            modifyHTMLLinked(obj.followers_count[i].automatic, obj.followers_count[i].followers, obj.followers_count[i].timestamp)
+        }
+      } else { tempo++ }
+      if (count == 0) {
+        count = 1
+        this.open('GET', url + platforma + actiune + 'stats', true)
+        this.send()
+      }
+    }
+  
+    invocation.send(requestDataLogin)
+  }
+  
+  function modifyHTMLLinked(automatic, followers, timestamp) {
+    var elem = document.createElement('div')
+    var heading = document.createElement('h4')
+    var automatic_p = document.createElement('p')
+    var followers_p = document.createElement('p')
+    var timestamp_p = document.createElement('p')
+  
+    var heading_n = document.createTextNode('Your daily followers count: ')
+    var automatic_n = document.createTextNode('Automatic: ' + automatic)
+    var followers_n = document.createTextNode('Number of followers: ' + followers)
+    var timestamp_n = document.createTextNode('Timestamp: ' + timestamp)
+  
+    heading.appendChild(heading_n)
+    automatic_p.appendChild(automatic_n)
+    followers_p.appendChild(followers_n)
+    timestamp_p.appendChild(timestamp_n)
+  
+    elem.appendChild(heading)
+    elem.appendChild(automatic_p)
+    elem.appendChild(followers_p)
+    elem.appendChild(timestamp_p)
+  
+    var original = document.getElementById('content-area')
+    original.append(elem)
+  }
+  
+  function removeChildren() {
+    const myNode = document.getElementById('content-area')
+    while (myNode.lastElementChild) {
+      myNode.removeChild(myNode.lastElementChild)
+    }
+  }
